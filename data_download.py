8dbbf6c1caafed5ee3e2ad865348c440ed618a5a
aerospacegirl97 <w.dziarnowska@gmail.com>
2022-03-10 17:20:23 +0100

    Made script easier to understand

--- data_download.py
+++ data_download.py
@@ -1,9 +1,15 @@
 # Reading an excel file using Python
 import csv
  
-# Give the location of the file
+######### Hi Krzysztof and Daniel, here's what you should do ##########
+# Change the urls path to your local path of the query_videoURLs.csv file
+# Change the titles path to your local path of the:
+# - video_name_train.csv file for TRAINIG data
+# - video_name_test.csv file for TESTING data
+# - video_name_val.csv file for VALIDATION data
+# And that's it! The code will save a text file called set_urls.txt
 urls = open(r"C:\Users\Weronika\Documents\GitHub\DL_Reproducibility_Project\dataset\videos\query_videoURLs.csv","r", encoding="Latin-1")
-titles_train = open(r"C:\Users\Weronika\Documents\GitHub\DL_Reproducibility_Project\dataset\videos\video_name_train.csv","r", encoding="Latin-1")
+titles = open(r"C:\Users\Weronika\Documents\GitHub\DL_Reproducibility_Project\dataset\videos\video_name_train.csv","r", encoding="Latin-1")
 
 urls_read = csv.reader(urls)
 urls_list = []
@@ -15,26 +21,20 @@ for row in urls_read:
 urls_list = urls_list[1:]
 all_titles_list = all_titles_list[1:]
 
-titles_train_read = csv.reader(titles_train)
-titles_train_list = []
-for row in titles_train_read:
-    titles_train_list.append(row[0])
+titles_read = csv.reader(titles)
+titles_list = []
+for row in titles_read:
+    titles_list.append(row[0])
     
-titles_train_list = titles_train_list[1:]
+titles_list = titles_list[1:]
 
-train_urls = []
+set_urls = []
 for i in range(190):
-    if all_titles_list[i] in titles_train_list:
-        train_urls.append(urls_list[i])
-
-practice_urls = train_urls[:2]        
-practice_txt = open("practice.txt", "w")
-for element in practice_urls:
-    practice_txt.write(element + "\n")
-practice_txt.close()
+    if all_titles_list[i] in titles_list:
+        set_urls.append(urls_list[i])
         
-train_urls_txt = open("train_urls.txt", "w")
-for element in train_urls:
-    train_urls_txt.write(element + "\n")
-train_urls_txt.close()
+set_urls_txt = open("set_urls.txt", "w")
+for element in set_urls:
+    set_urls_txt.write(element + "\n")
+set_urls_txt.close()
 
