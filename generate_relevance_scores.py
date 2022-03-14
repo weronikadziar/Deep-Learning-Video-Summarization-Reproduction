25ad83da6a50310eaf99e66fcec235d1c9a9c0c3
Krzysztof Garbowicz <kgar@netcompany.com>
2022-03-14 11:48:16 +0100

    Update generate_relevance_scores.py

--- generate_relevance_scores.py
+++ generate_relevance_scores.py
@@ -4,22 +4,15 @@ import pandas as pd
 import numpy as np
 from pyparsing import dict_of
 
-def generate_dict():
-    df = pd.read_csv("./dataset/annotations/query_frame_annotations_test.csv")
-    dict_of_frames = {}
-    for i, row in df.iterrows():
-        if row['imageURL'] in dict_of_frames:
-            dict_of_frames[row['imageURL']].append(row['Relevance'])
-        else:
-            dict_of_frames[row['imageURL']] = [row['Relevance']]
+df =pd.read_csv("./dataset/annotations/query_frame_annotations_test.csv")
+dict_of_frames = {}
 
-def generate_scores_simple_count(dict_of_frames):
-    res = [] 
-    for rec in dict_of_frames:
-        res.append((rec, np.argmax(np.bincount(dict_of_frames[rec]))))
-    return res
+for i, row in df.iterrows():
+    if row['imageURL'] in dict_of_frames:
+        dict_of_frames[row['imageURL']].append(row['Relevance'])
+    else:
+        dict_of_frames[row['imageURL']] = [row['Relevance']]
 
-dict_of_frames = generate_dict()
-print(dict_of_frames)
-res = generate_scores_simple_count(dict_of_frames)
-print(res)
+for rec in dict_of_frames:
+    print("{} scores: {}".format(rec, dict_of_frames[rec]))
+    print("{} scores: {}".format(rec, np.argmax(np.bincount(dict_of_frames[rec]))))
